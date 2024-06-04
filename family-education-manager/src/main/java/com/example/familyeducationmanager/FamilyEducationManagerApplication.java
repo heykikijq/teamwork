ada4e974bf868927536ef4a8739592c5d078ef67
heykikijq <126369689+heykikijq@users.noreply.github.com>
2024-06-04 15:39:20 +0800

    Add files via upload

---
+++ family-education-manager/src/main/java/com/example/familyeducationmanager/FamilyEducationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.familyeducationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyEducationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyEducationManagerApplication.class, args);
+    }
+
+}
---
+++ family-education-manager/src/test/java/com/example/familyeducationmanager/FamilyEducationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.familyeducationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyEducationManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
